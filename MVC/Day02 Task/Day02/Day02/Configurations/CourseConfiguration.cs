e0a560ee7581a88539c9627930cb4d434021ff44
AbdoMahdy1 <abdelsalammahdy1@gmail.com>
2025-09-22 18:00:56 +0300

    Day02 MVC

---
+++ MVC/Day02 Task/Day02/Day02/Configurations/CourseConfiguration.cs
@@ -0,0 +1,40 @@
+﻿using Day02.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Day02.Configurations
+{
+    public class CourseConfiguration : IEntityTypeConfiguration<Course>
+    {
+        public void Configure(EntityTypeBuilder<Course> C)
+        {
+            C.ToTable("Courses");
+            C.HasKey(c => c.Id);
+
+            C.Property(c => c.Id)
+                .UseIdentityColumn(1, 1);
+
+            C.Property(c => c.Name)
+                .IsRequired(true)
+                .HasColumnType("varchar")
+                .HasColumnName("CrsName")
+                .HasMaxLength(50);
+
+            C.Property(c => c.MinDegree)
+                .IsRequired(true);
+            
+            C.Property(c => c.Degree)
+                .IsRequired(true);
+
+            C.HasMany(c => c.Teachers)
+                .WithOne(t => t.Course)
+                .HasForeignKey(t => t.CourseId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            C.HasMany(c => c.Results)
+                .WithOne(r => r.Course)
+                .HasForeignKey(r => r.CourseId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
---
+++ MVC/Day02 Task/Day02/Day02/Configurations/ResultConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Day02.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Day02.Configurations
+{
+    public class ResultConfiguration : IEntityTypeConfiguration<StuCrsResult>
+    {
+        public void Configure(EntityTypeBuilder<StuCrsResult> R)
+        {
+            R.ToTable("StuCrsResults");
+            R.HasKey(r => new { r.StuId, r.CourseId });
+
+            R.Property(r => r.Grade)
+                .IsRequired(true);
+        }
+    }
+}
